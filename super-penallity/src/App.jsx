2c2f2b44b37b1030a1589dde7c5c1842cd0b3bac
Danil <henson1330@gmail.com>
2026-04-26 19:35:58 +0400

    игра пенальти собрана под версель

--- super-penallity/src/App.jsx
+++ super-penallity/src/App.jsx
@@ -1,20 +1,8 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import GamePage from './pages/GamePage';
-import ProfilePage from './pages/ProfilePage';
+import React from 'react'
+import GamePage from './pages/GamePage'
 
 function App() {
-  return (
-    <HashRouter>
-      <div className="bg-gray-900 min-h-screen text-white">
-        <Routes>
-          <Route path="/" element={<GamePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<GamePage />} />
-        </Routes>
-      </div>
-    </HashRouter>
-  );
+  return <GamePage />
 }
 
-export default App;
+export default App
--- super-penallity/vite.config.js
+++ super-penallity/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/super-penallity/',
-  publicDir: false,
   build: {
-    outDir: '.',
-    assetsDir: '',
-    emptyOutDir: false,
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
+  server: {
+    port: 3000,
+    host: true
+  }
 })
-
