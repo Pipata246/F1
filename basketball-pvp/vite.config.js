4181a7a4222b627a12b0e387acb556e4b764b530
Danil <henson1330@gmail.com>
2026-04-28 20:30:16 +0400

    исправлена архитектура баскетбола

--- basketball-pvp/vite.config.js
+++ basketball-pvp/vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/basketball-pvp/',
-  publicDir: false,
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
